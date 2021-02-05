044a81470288f63c2decb50606dd34c31275b38e
Janakitti <janakittiratana@gmail.com>
2021-02-04 22:19:57 -0500

    Fetch link data from server

--- components/links-window.js
+++ components/links-window.js
@@ -1,35 +1,36 @@
-import {useState, useEffect} from "react";
-import LinkItem from './link-item';
-import styled from 'styled-components';
+import { useState, useEffect } from "react";
+import LinkItem from "./link-item";
+import styled from "styled-components";
+import axios from "axios";
 
-const LinksWindow = (selected) => {
-    const [links, setLinks] = useState([]);
+const LinksWindow = ({ selected }) => {
+  const [links, setLinks] = useState([]);
 
-    useEffect(async () => {
-        fetch('links-data.json', {
-            headers : {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        })
-            .then(res => res.json())
-            .then(data => setLinks(data))
-            .catch(ex => console.log(ex));
-    }, [selected]);
+  useEffect(async () => {
+    try {
+      const res = await axios.get(
+        "http://localhost:4000/api/links/" + selected,
+        {
+          method: "get",
+          withCredentials: true,
+        }
+      );
+      setLinks(res.data);
+    } catch (ex) {
+      console.log(ex);
+    }
+  }, [selected]);
 
-    const linkItems = links.map((link, idx) => <LinkItem key={idx} _id={link._id} label={link.label} url={link.label} />);
+  const linkItems = links.map((link, idx) => (
+    <LinkItem key={idx} _id={link._id} label={link.label} url={link.label} />
+  ));
 
-    return (
-        <Window>
-            {linkItems}
-        </Window>
-    )
-
-}
+  return <Window>{linkItems}</Window>;
+};
 
 const Window = styled.div`
-    height: 100%;
-    padding: 3em 2em;
+  height: 100%;
+  padding: 3em 2em;
 `;
 
 export default LinksWindow;
