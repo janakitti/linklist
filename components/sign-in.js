2558f76420e96a30d1c299dff60bf3692e6ca18b
Janakitti <janakittiratana@gmail.com>
2021-02-04 21:48:37 -0500

    Fetch data from server

--- components/sign-in.js
+++ components/sign-in.js
@@ -1,123 +1,135 @@
-import styled from 'styled-components';
-import WelcomeLayout from './welcome-layout';
-import {useState} from "react";
-import axios from 'axios';
+import styled from "styled-components";
+import WelcomeLayout from "./welcome-layout";
+import { useState } from "react";
+import axios from "axios";
 import { Router, useRouter } from "next/router";
 
-const SignIn = ({setState}) => {
-    const router = useRouter();
-    const [user, setUser] = useState({
-        email: '',
-        password: '',
-    });
-    
-    function handleChange(event) {
-        const {name, value} = event.target;
-        setUser({ ...user, [name]: value });
-        console.log(user);
+const SignIn = ({ setState }) => {
+  const router = useRouter();
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setUser({ ...user, [name]: value });
+    console.log(user);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const creds = {
+      email: user.email,
+      password: user.password,
+    };
+
+    try {
+      const res = await axios.post("http://localhost:4000/api/auth/", creds, {
+        withCredentials: true,
+      });
+      console.log(res);
+      router.push("/links");
+    } catch (ex) {
+      console.log(ex);
     }
+  }
 
-    async function handleSubmit(event) {
-        event.preventDefault();
-
-        const creds = {
-            email: user.email,
-            password: user.password
-        };
-
-        try {
-            await axios.post('http://localhost:4000/api/auth/', creds, {
-                withCredentials: true
-            });
-            router.push("/links");
-        } catch (ex) {
-            console.log(ex);
-        }
-    }
-
-    return (
-        <WelcomeLayout>
-            <Container>
-                <Title>
-                    linklist
-                </Title>
-                <Subtitle>
-                    Sign in for better bookmarking
-                </Subtitle>
-                <form onSubmit={handleSubmit}>
-                    <Input type="email" name="email" placeholder="Email" value={user.email} onChange={handleChange}></Input>
-                    <Input type="password" name="password" placeholder="Password" value={user.password} onChange={handleChange}></Input>
-                    <Button type="submit">Sign in</Button>
-                </form>
-                <Info>Don't have an account? <b onClick={() => setState("SignUp")}>Sign up</b></Info>
-            </Container>
-        </WelcomeLayout>
-    )
-}
+  return (
+    <WelcomeLayout>
+      <Container>
+        <Title>linklist</Title>
+        <Subtitle>Sign in for better bookmarking</Subtitle>
+        <form onSubmit={handleSubmit}>
+          <Input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={user.email}
+            onChange={handleChange}
+          ></Input>
+          <Input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={user.password}
+            onChange={handleChange}
+          ></Input>
+          <Button type="submit">Sign in</Button>
+        </form>
+        <Info>
+          Don't have an account?{" "}
+          <b onClick={() => setState("SignUp")}>Sign up</b>
+        </Info>
+      </Container>
+    </WelcomeLayout>
+  );
+};
 
 const Info = styled.p`
-    margin: 2em 0 0 0;
+  margin: 2em 0 0 0;
 
-    font-family: Poppins;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 14px;
-    line-height: 21px;
-    text-align: center;
+  font-family: Poppins;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 21px;
+  text-align: center;
 
-    color: #767676;
+  color: #767676;
 
-    cursor: pointer;
+  cursor: pointer;
 `;
 
 const Container = styled.div`
-    position: relative;
-    align-items: center;
-    text-align: center;
+  position: relative;
+  align-items: center;
+  text-align: center;
 `;
 
 const Title = styled.h1`
-    font-family: Poppins;
-    font-weight: 700;
-    font-size: 2em;
+  font-family: Poppins;
+  font-weight: 700;
+  font-size: 2em;
 `;
 
 const Subtitle = styled.h2`
-    font-family: Poppins;
-    font-weight: 300;
-    font-size: 1em;
+  font-family: Poppins;
+  font-weight: 300;
+  font-size: 1em;
 
-    margin: 0 0 2em 0;
+  margin: 0 0 2em 0;
 `;
 
 const Input = styled.input`
-    width: 30em;
-    height: 3em;
+  width: 30em;
+  height: 3em;
 
-    padding: 0 2em;
-    margin: 0.5em 0;
+  padding: 0 2em;
+  margin: 0.5em 0;
 
-    background: #FFFFFF;
-    border: none;
-    border-radius: 56.5px;
+  background: #ffffff;
+  border: none;
+  border-radius: 56.5px;
 `;
 
 const Button = styled.button`
-    width: 30em;
-    height: 3em;
+  width: 30em;
+  height: 3em;
 
-    padding: 0 2em;
-    margin: 0.5em 0;
+  padding: 0 2em;
+  margin: 0.5em 0;
 
-    background: #DEC800;
-    box-shadow: 4px 4px 24px rgba(0, 0, 0, 0.15);
-    border: none;
-    border-radius: 56.5px;
+  background: #dec800;
+  box-shadow: 4px 4px 24px rgba(0, 0, 0, 0.15);
+  border: none;
+  border-radius: 56.5px;
 
-    color: #FFFFFF;
-    font-family: Poppins;
-    font-weight: 300;
-    font-size: 1em;
+  color: #ffffff;
+  font-family: Poppins;
+  font-weight: 300;
+  font-size: 1em;
 `;
 
 export default SignIn;
--- pages/links.js
+++ pages/links.js
@@ -1,70 +1,76 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import ListsPanel from '../components/lists-panel';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import ListsPanel from "../components/lists-panel";
 import LinksWindow from "../components/links-window";
 import styled from "styled-components";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Links() {
-    const [user, setUser] = useState({
-        username: "",
-        email: "",
-        listsIds: ['o']
-    });
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    listsIds: ["o"],
+  });
 
-    const [lists, setLists] = useState([]);
+  const [lists, setLists] = useState([]);
 
-    const [selected, setSelected] = useState("");
+  const [selected, setSelected] = useState("");
 
-    useEffect(async () => {
-        fetch('user-data.json', {
-            headers : {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        })
-            .then(res => res.json())
-            .then(data => setUser(data))
-            .catch(ex => console.log(ex));
-    }, []);
-
-    useEffect(async() => {
-        fetch('lists-data.json', {
-            headers : {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        })
-            .then(res => res.json())
-            .then(data => setLists(data))
-            .catch(ex => console.log(ex));
-    }, [user.listIds]);
-
-    const handleSelect = (id) => {
-        setSelected(id);
+  useEffect(async () => {
+    try {
+      const res = await axios.get("http://localhost:4000/api/users/me", {
+        method: "get",
+        withCredentials: true,
+      });
+      setUser(res.data);
+    } catch (ex) {
+      console.log(ex);
     }
+  }, []);
 
-    return(
-        <FullRow>
-            <FullCol xs={3}>
-                <ListsPanel username={user.username} lists={lists} handleSelect={handleSelect} selected={selected}/>
-            </FullCol>
-            <FullCol xs={9}>
-                <LinksWindow selected={selected}/>
-            </FullCol>
-        </FullRow>
-    )
+  useEffect(async () => {
+    try {
+      const res = await axios.get("http://localhost:4000/api/lists", {
+        method: "get",
+        withCredentials: true,
+      });
+      setLists(res.data);
+    } catch (ex) {
+      console.log(ex);
+    }
+  }, [user.listIds]);
+
+  const handleSelect = (id) => {
+    setSelected(id);
+  };
+
+  return (
+    <FullRow>
+      <FullCol xs={3}>
+        <ListsPanel
+          username={user.username}
+          lists={lists}
+          handleSelect={handleSelect}
+          selected={selected}
+        />
+      </FullCol>
+      <FullCol xs={9}>
+        <LinksWindow selected={selected} />
+      </FullCol>
+    </FullRow>
+  );
 }
 
 const FullCol = styled(Col)`
-    height: 100%;
-    width: 100%;
-    padding: 0;
+  height: 100%;
+  width: 100%;
+  padding: 0;
 `;
 
 const FullRow = styled(Row)`
-    height: 100%;
-    width: 100%;
-    margin: 0;
+  height: 100%;
+  width: 100%;
+  margin: 0;
 `;
