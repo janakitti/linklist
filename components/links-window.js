411d6e88dd3141e1c6fd33df4c37d57c96aa0fa7
Janakitti <janakittiratana@gmail.com>
2021-02-06 14:43:59 -0500

    Create add new link form

--- components/links-window.js
+++ components/links-window.js
@@ -5,8 +5,21 @@ import axios from "axios";
 
 const LinksWindow = ({ selected }) => {
   const [links, setLinks] = useState([]);
+  const [newLink, setNewLink] = useState({
+    label: "",
+    url: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNewLink({ ...newLink, [name]: value });
+  }
 
   useEffect(async () => {
+    fetchLinks();
+  }, [selected]);
+
+  const fetchLinks = async () => {
     try {
       const res = await axios.get(
         "http://localhost:4000/api/links/" + selected,
@@ -19,18 +32,95 @@ const LinksWindow = ({ selected }) => {
     } catch (ex) {
       console.log(ex);
     }
-  }, [selected]);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const res = await axios.post(
+        "http://localhost:4000/api/links/" + selected,
+        newLink,
+        {
+          withCredentials: true,
+        }
+      );
+      console.log(res);
+      setNewLink({ label: "", url: "" });
+      fetchLinks();
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
 
   const linkItems = links.map((link, idx) => (
     <LinkItem key={idx} _id={link._id} label={link.label} url={link.label} />
   ));
 
-  return <Window>{linkItems}</Window>;
+  return (
+    <Window>
+      {linkItems}
+      <NewLinkContainer>
+        <form onSubmit={handleSubmit}>
+          <Input
+            type="text"
+            name="label"
+            placeholder="Label"
+            value={newLink.label}
+            onChange={handleChange}
+          ></Input>
+          <Input
+            type="text"
+            name="url"
+            placeholder="URL"
+            value={newLink.url}
+            onChange={handleChange}
+          ></Input>
+          <Button type="submit">+</Button>
+        </form>
+      </NewLinkContainer>
+    </Window>
+  );
 };
 
 const Window = styled.div`
   height: 100%;
   padding: 3em 2em;
+`;
+
+const NewLinkContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Input = styled.input`
+  flex-grow: 4;
+  height: 3em;
+
+  padding: 0 2em;
+  margin: 0.5em 1em 0.5em 0;
+
+  background-color: #f2f2f2;
+  border: none;
+  border-radius: 56.5px;
+`;
+
+const Button = styled.button`
+  width: 5em;
+  height: 3em;
+
+  padding: 0 2em;
+  margin: 0.5em 0;
+
+  background: #dec800;
+  box-shadow: 4px 4px 24px rgba(0, 0, 0, 0.15);
+  border: none;
+  border-radius: 56.5px;
+
+  color: #ffffff;
+  font-family: Poppins;
+  font-weight: 300;
+  font-size: 1em;
 `;
 
 export default LinksWindow;
