2dc4d3a8edefbbdb8b8853ed56c9a4aba978733c
janakitti <janakittiratana@gmail.com>
2021-12-29 18:08:54 -0500

    Update Button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,14 +1,22 @@
 import { Color } from "../utils/theme";
 
 export interface IButtonProps {
-  color: Color;
+  color: string;
   onClick: () => void;
 }
 
-const Button: React.FC<IButtonProps> = ({ color, children }) => (
-  <button className="py-2 px-4 my-2 bg-gold hover:bg-gold-dark rounded-full">
-    <p className="text-white">{children}</p>
-  </button>
-);
+const Button: React.FC<IButtonProps> = ({ color, children }) => {
+  let buttonClasses;
+  switch (color) {
+    case Color.primary:
+      buttonClasses =
+        "py-2 px-4 my-2 h-10 flex items-center justify-center bg-primary hover:bg-primary-dark rounded-full";
+  }
+  return (
+    <button className={buttonClasses}>
+      <p className="text-white">{children}</p>
+    </button>
+  );
+};
 
 export default Button;
