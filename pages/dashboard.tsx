4fe74126ea504b915fb0a526c3c6b5f8e10df4c9
janakitti <janakittiratana@gmail.com>
2021-12-30 18:04:33 -0500

    Wrap Dashboard with withAuth

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,26 +1,10 @@
 import axios from "axios";
 import type { NextPage } from "next";
 import { useEffect } from "react";
+import { withAuth } from "../components/WithAuth";
 import { config } from "../utils/config";
 
 const Dashboard: NextPage = () => {
-  useEffect(() => {
-    (async () => {
-      try {
-        const res = await axios.get(config.apiUrl + "/users/me", {
-          method: "get",
-          withCredentials: true,
-        });
-        // const res = await axios.get("api/me", {
-        //   method: "get",
-        //   withCredentials: true,
-        // });
-        console.log(res);
-      } catch (ex) {
-        console.log(ex);
-      }
-    })();
-  }, []);
   return (
     <div className="grid-container grid grid-cols-1 lg:grid-cols-6 h-screen">
       <div className="item1 col-span-1 bg-warn h-full">1</div>
@@ -30,4 +14,4 @@ const Dashboard: NextPage = () => {
   );
 };
 
-export default Dashboard;
+export default withAuth(Dashboard);
