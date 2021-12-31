033cea9d6e0e05686f5c11ffd73f22683769fc32
janakitti <janakittiratana@gmail.com>
2021-12-30 19:49:36 -0500

    Display username on ProfilePanel

--- components/WithAuth.tsx
+++ components/WithAuth.tsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import Router from "next/router";
 import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { resetUser, setUser } from "../redux/actions";
 import { config } from "../utils/config";
 import { Color, Size } from "../utils/theme";
 import Loader from "./Loader";
@@ -8,18 +10,21 @@ import Loader from "./Loader";
 export function withAuth<P>(WrappedComponent: React.ComponentType<P>) {
   const ComponentWithAuth = (props: P) => {
     const [verified, setVerified] = useState(false);
+    const dispatch = useDispatch();
 
     useEffect(() => {
       (async () => {
         try {
-          await axios.get(config.apiUrl + "/users/me", {
+          let res = await axios.get(config.apiUrl + "/users/me", {
             method: "get",
             withCredentials: true,
           });
           setVerified(true);
+          dispatch(setUser(res.data));
         } catch (ex) {
           console.log(ex);
           setVerified(false);
+          dispatch(resetUser());
           Router.push("/sign-in");
         }
       })();
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,15 +1,18 @@
 import axios from "axios";
 import type { NextPage } from "next";
 import { useEffect } from "react";
+import ProfilePanel from "../components/ProfilePanel";
 import { withAuth } from "../components/WithAuth";
 import { config } from "../utils/config";
 
 const Dashboard: NextPage = () => {
   return (
     <div className="grid-container grid grid-cols-1 lg:grid-cols-6 h-screen">
-      <div className="item1 col-span-1 bg-warn h-full">1</div>
-      <div className="item2 col-span-3 bg-primary h-full">2</div>
-      <div className="item2 col-span-2 bg-light h-full">2</div>
+      <div className="item1 col-span-1 h-full">
+        <ProfilePanel />
+      </div>
+      <div className="item2 col-span-3 h-full">2</div>
+      <div className="item2 col-span-2 h-full">2</div>
     </div>
   );
 };
